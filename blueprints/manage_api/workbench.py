aa17012826e4242beed3143a6fb1aa25e1312a09
asdevao <1097802349@qq.com>
2025-01-05 22:05:29 +0800

    更新workbench.py文件中的路由函数路径

--- blueprints/manage_api/workbench.py
+++ blueprints/manage_api/workbench.py
@@ -1,3 +1,11 @@
+'''
+Author: asdevao 1097802349@qq.com
+Date: 2025-01-02 17:23:15
+LastEditors: asdevao 1097802349@qq.com
+LastEditTime: 2025-01-05 22:04:02
+FilePath: \新建文件夹\mini_program_backend\blueprints\manage_api\workbench.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
 from flask import Blueprint, session
 from ..Models.OrderModel import Order
 from ..utils.response_util import ResponseUtil
@@ -6,7 +14,7 @@ from ..Models.UserModel import ManageUser
 bp = Blueprint('workbeach', __name__)
 
 
-@bp.route('/', methods=['GET'])
+@bp.route('/info', methods=['GET'])
 def getWorkBeachData():
     # 使用filter_by来筛选name匹配且department不等于“小程序用户”
     order_count = Order.query.count()
--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@
 Author: asdevao 1097802349@qq.com
 Date: 2024-10-25 16:02:16
 LastEditors: asdevao 1097802349@qq.com
-LastEditTime: 2025-01-05 21:10:39
+LastEditTime: 2025-01-05 22:00:07
 FilePath: \apps\settings.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
